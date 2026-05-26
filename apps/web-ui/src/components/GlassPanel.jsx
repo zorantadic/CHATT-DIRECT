a4f4ef5528eb4d432914ecd20d800b532dc91ab3
Zoran Tadic <zoran@itprofessional.org>
2026-05-26 10:08:26 -0500

    Remove web app work

--- apps/web-ui/src/components/GlassPanel.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function GlassPanel({ as: Component = "section", children, className = "", ...props }) {
-  return (
-    <Component className={`glassPanel${className ? ` ${className}` : ""}`} {...props}>
-      {children}
-    </Component>
-  );
-}
--- apps/web-ui/src/components/StatusPill.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function StatusPill({ children, tone = "warn" }) {
-  return <span className={`pill ${tone}`}>{children}</span>;
-}
